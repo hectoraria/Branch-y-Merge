6ee690bd76fd86e0bffa11445be1ba90d91928d8
eduarnesto <eduardo.arnesto@iesnervion.es>
2024-01-19 12:54:15 +0100

    Modificaiones Edu

--- EjercicioGit/src/ejercicio/EjercicioGit.java
+++ EjercicioGit/src/ejercicio/EjercicioGit.java
@@ -5,7 +5,7 @@ public class EjercicioGit {
 	public static void main(String[] args) {
 		System.out.println("Hola");
 		System.out.println("Hola soy Hector");
-
+		System.out.println("Hola soy Edu");
 	}
 
 }
